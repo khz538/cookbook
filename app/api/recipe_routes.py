0ed72e7b3de512f4cae391f5f60f62cb4096fb99
kevin <zhang.kevin.hua@gmail.com>
2022-08-31 05:34:44 -0500

    recipe edit query changed to include ingredients and steps

--- app/api/recipe_routes.py
+++ app/api/recipe_routes.py
@@ -57,21 +57,26 @@ def create_recipe():
 @recipe_routes.route('/<int:recipe_id>/edit', methods=['PUT'])
 @login_required
 def edit_recipe(recipe_id):
-    recipe = Recipe.query.get(recipe_id)
-    if recipe is not None:
-        recipe_dict = recipe.to_dict()
-        if recipe_dict['user_id'] != current_user.id:
-            return {'errors': ['You are not authorized to edit this recipe']}, 401
-        else:
-            form = RecipeForm()
-            form['csrf_token'].data = request.cookies['csrf_token']
-            if form.validate_on_submit():
-                for k in form.data:
-                    if k != 'csrf_token':
-                        setattr(recipe, k, form.data[k])
-                db.session.commit()
-                return recipe.to_dict()
-            else:
-                return {'errors': validation_errors_to_error_messages(form.errors)}, 401
-    else:
-        return {'errors': ['Recipe not found']}, 404
+    # Need to return the recipe along with the ingredients and steps
+    recipe = db.session.query(Recipe)\
+        .options(db.joinedLoad(Recipe.ingredients))\
+        .options(db.joinedLoad(Recipe.steps))\
+        .get(recipe_id)
+    print(recipe.to_dict())
+    # if recipe is not None:
+    #     recipe_dict = recipe.to_dict()
+    #     if recipe_dict['user_id'] != current_user.id:
+    #         return {'errors': ['You are not authorized to edit this recipe']}, 401
+    #     else:
+    #         form = RecipeForm()
+    #         form['csrf_token'].data = request.cookies['csrf_token']
+    #         if form.validate_on_submit():
+    #             for k in form.data:
+    #                 if k != 'csrf_token':
+    #                     setattr(recipe, k, form.data[k])
+    #             db.session.commit()
+    #             return recipe.to_dict()
+    #         else:
+    #             return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+    # else:
+    #     return {'errors': ['Recipe not found']}, 404
