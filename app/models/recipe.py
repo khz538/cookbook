788ec7929849013cdc2d2c17534611c0d2aa94fc
kevin <zhang.kevin.hua@gmail.com>
2022-08-31 00:55:05 -0500

    recipe model columns added and linked to user model

---
+++ app/models/recipe.py
@@ -0,0 +1,17 @@
+from time import time
+from .db import db
+from flask_login import UserMixin
+
+
+class Recipe(db.Model, UserMixin):
+    __tablename__ = 'recipes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    time = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    yield_servings = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(255))
+
+    user = db.relationship('User', back_populates='recipes', foreign_keys=[user_id])
