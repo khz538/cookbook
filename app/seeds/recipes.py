ea2d4375e718a8eed142978aa758ed37e2068daa
kevin <zhang.kevin.hua@gmail.com>
2022-09-06 10:22:46 -0500

    changed recipe migration file time column from int to string

--- app/seeds/recipes.py
+++ app/seeds/recipes.py
@@ -8,7 +8,7 @@ def seed_recipes():
         description='The Three Sisters — corn, beans and squash — are foundational foods of the Haudenosaunee people. This recipe is an Indigenous-inspired twist on falafel that brings together the sweetness of squash with savory red beans and cornmeal. Combined with an easy raspberry aioli, these nutritious patties can be eaten alone, tossed in salads or tucked into sandwiches.',
         time='2 hours',
         yield_servings = 4,
-        image_url='https://static01.nyt.com/images/2022/08/31/dining/31GARDENSrex2-1/merlin_211376418_8eaf5162-49a1-4a95-a3f7-46e89a62dadf-articleLarge.jpg?w=1280&q=75',
+        image_url='https://res.cloudinary.com/khz538/image/upload/v1661845151/cld-sample-4.jpg',
         user_id=1
     )
 
