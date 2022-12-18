53ff1c9826bd34913bf7ef2dc5623967e821bd47
Kevin Zhang <zhang.kevin.hua@gmail.com>
2022-12-18 04:07:07 -0600

    added botocore and boto3 to requirements/Pipfile

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -24,3 +24,15 @@ def search(search_term):
         recipes_list.append(recipe_dict)
     # return {'recipes': [recipe.to_dict() for recipe in recipes]}
     return {'recipes': recipes_list}
+
+# Search descriptions
+@search_routes.route('/description/<search_term>/')
+def search_description(search_term):
+    recipes = db.session.query(Recipe).filter(Recipe.description.ilike(f'%{search_term}%')).all()
+    recipes_list = list()
+    for recipe in recipes:
+        recipe_dict = recipe.to_dict()
+        user = User.query.get(recipe_dict['user_id'])
+        recipe_dict['user'] = user.to_dict()
+        recipes_list.append(recipe_dict)
+    return {'recipes': recipes_list}
